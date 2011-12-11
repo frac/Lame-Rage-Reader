c22c3e33c3a9c6819e3e3f7b6c3b77c84debc48a
adriano petrich <petrich@gmail.com>
2011-12-11 20:32:05 -0200

    nao rever coisas velhas

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,19 +1,29 @@
-var page = 0;
-var nextURL = "";
-var items;
-var loading = false;
-//var base_url = "http://www.reddit.com/r/wtf/new/.json?sort=new"
-//var base_url = "http://www.reddit.com/r/pics/new/.json?sort=new";
-var base_url = "http://www.reddit.com/r/fffffffuuuuuuuuuuuu/.json",
-    seen = {};
+var page = 0,
+    nextURL = "",
+    items,
+    loading = false,
+    //base_url = "http://www.reddit.com/r/wtf/new/.json?sort=new"
+    //base_url = "http://www.reddit.com/.json?feed=7e73de803f62396bfe33e75fd1aa49a48958cdd0&user=fractalk".
 
-var pageLoadSuccess = function(response) {
+    //base_url = "http://www.reddit.com/r/pics/new/.json?sort=new",
+    base_url = "http://www.reddit.com/r/fffffffuuuuuuuuuuuu/.json",
+    seen = [],
+
+
+
+
+page_error = function(res, stat) {
+    console.log("erro ao pegar pagina "+ res);
+},
+
+
+pageLoadSuccess = function(response) {
     items = [];
     console.log("jsonp after " + response.data.after );
     $.each(response.data.children, function(index, val) {
-        //console.log("objeto"+ val.data.domain)
+        console.log("peguei "+ val.data.id);
         if (val.data.domain === "imgur.com") {
-            if ( !seen[val.data.permalink] ) {
+            if ( !include(seen, val.data.id) ) {
                 var imgURL = val.data.url;
 
                 var extPos = val.data.url.lastIndexOf(".");
@@ -22,13 +32,18 @@ var pageLoadSuccess = function(response) {
                 }
 
                 imgURL = imgURL + ".png";
-                items.push('<li class="comicContainer" id="' + index + '"><div class="comicHeader"><h3 class="comicTitle">' + val.data.title + '</h3><a class="commentsLink" href="http://www.reddit.com' + val.data.permalink + '">Comments (' + val.data.num_comments + ')</a></div><img src="' + imgURL + '"/></li>');
-                seen[val.data.permalink] = true;
+                items.push('<li class="comicContainer" id="' + index + '"><div class="comicHeader"><h3 class="comicTitle">' + val.data.title + '</h3><a class="commentsLink" href="http://www.reddit.com' + val.data.permalink + '">Comments (' + val.data.num_comments + ')</a></div><img src="' + imgURL + '" class="the_comics"/></li>');
+                append(seen, val.data.id);
             } else {
                 console.log("ja vi o "+ val.data.permalink);
             }
         }
     });
+    console.log("salvando "+ seen.length +" itens");
+    if (seen.length > 200){
+        seen.splice(0,seen.length - 200);
+    }
+    window.localStorage.setItem("seen", seen);
 
     page++;
     var ulID = 'page' + page;
@@ -40,19 +55,54 @@ var pageLoadSuccess = function(response) {
 
     nextURL = base_url + "?count=25&after=" + response.data.after;
     loading = false;
+    if (items.length < 3){
+        console.log("Nao achei comics o suficiente");
+        load_comics();    
+    }
+
+};
+clear_history =  function() {
+    console.log("limpando a bagaca");
+    seen = [];
+    window.localStorage.clear();
+    window.location.reload();
+};
+include = function(arr,obj) {
+    return (arr.indexOf(obj) != -1);
+};
+append = function(arr,obj) {
+    arr.push(obj);
+};
+load_comics = function() {
+    loading = true;
+    $.ajax({
+        type: 'GET',
+        url: nextURL,
+        jsonp: 'jsonp',
+        dataType: 'jsonp',
+        success: pageLoadSuccess,
+        error: page_error
+    });
 };
 
+//$(document).ready(
 
-$(document).ready(function(){
-	$.ajax({
-		type: 'GET',
-		//url: 'http://www.reddit.com/r/fffffffuuuuuuuuuuuu/.json',
-		url: base_url,
-		jsonp: 'jsonp',
-		dataType: 'jsonp',
-		success: pageLoadSuccess
-	});
-});
+onDeviceReady = function() {
+    //db = window.openDatabase("fooo", "0.1", "fooDB", 1000000);
+    console.log("iniciando");
+    seen = window.localStorage.getItem("seen") ;
+    if ( seen === undefined || seen === null ){
+        seen = [];
+    } else {
+        seen = seen.split(",");       
+    }
+
+    console.log("tenho armazenado "+ (typeof seen) );
+    $("#clear").click(function (e){ e.preventDefault(); clear_history();  });
+    $("#reload").click(function (e){ window.location.reload();  });
+    nextURL = base_url;
+    load_comics();
+};
 
 $(window).scroll(function () {
 	if (!loading) {
@@ -74,14 +124,7 @@ $(window).scroll(function () {
 		});
 
 		if (distanceFromBottom < last3ComicsHeight) {
-			loading = true;
-			$.ajax({
-				type: 'GET',
-				url: nextURL,
-				jsonp: 'jsonp',
-				dataType: 'jsonp',
-				success: pageLoadSuccess
-			});
+            load_comics();
 		}
 	}
 });
